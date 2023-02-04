cf399a168d8a3d09c56b70caca97d119d00bbc98
ronrk <ronrk11@gmail.com>
2023-02-04 07:55:43 +0200

    update the meta head tag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <meta
           name="description"
-          content="Ron Rock Rokkah Fullstack Frontend developer. create by Ron Rokkah 2023, my featured projects, about me page, and all projects seperate by categories"
+          content="Ron Rock Rokkah Fullstack Frontend developer portfolio , create by Ron Rokkah 2023, my featured projects, about me page, and all projects seperate by categories"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/assets/images/Crop@Shadow2@2x.png" />
